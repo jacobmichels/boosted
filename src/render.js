b8af7d64bed73cffa56803fafde317c3f4b97684
Jacob Michels <jacob.michels2025@gmail.com>
2020-04-09 04:42:05 -0400

    stop button clears request interval

--- src/render.js
+++ src/render.js
@@ -1,76 +1,90 @@
 const LCUConnector = require('lcu-connector');
+const https = require('https');
 const connector = new LCUConnector();
 const Base64 = require('js-base64').Base64;
 const request = require('postman-request');
 const remote = require('electron').remote;
 
+var interval;
+
 const customTitlebar = require('custom-electron-titlebar');
- 
+
 new customTitlebar.Titlebar({
     backgroundColor: customTitlebar.Color.fromHex('#212121'),
-    menu:null,
-    icon:"../resources/icons/lol-icon.png"
+    menu: null,
+    icon: "../resources/icons/lol-icon.png"
 });
 
-$(document).ready(function(){
+$(document).ready(function () {
     $('#stop-btn').addClass('disabled');
     $('#suspend-btn').addClass('disabled');
 })
 
-process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
+// process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
 
 connector.on('connect', (data) => {
     console.log(data);
-    document.getElementById('message').innerHTML="Successfully attached to game instance. Will now begin to poll for queue prompt.";
+    document.getElementById('message').innerHTML = "Successfully attached to game instance. Will now begin to poll for queue prompt.";
     $('#hook-btn').addClass('disabled');
-    let pw = Base64.encode("riot:"+data.password);
+    let pw = Base64.encode("riot:" + data.password);
     console.log(pw);
-    const options={
-        url:'https://127.0.0.1:'+data.port+'/lol-matchmaking/v1/ready-check',
+    var agentOptions;
+    var agent;
+
+    agentOptions = {
+        rejectUnauthorized: false
+    };
+
+    agent = new https.Agent(agentOptions);
+    const options = {
+        url: 'https://127.0.0.1:' + data.port + '/lol-matchmaking/v1/ready-check',
         headers: {
             'User-Agent': 'postman-request',
-            'Accept':'application/json',
-            'Authorization':'Basic '+pw,
+            'Accept': 'application/json',
+            'Authorization': 'Basic ' + pw,
         },
+        agent:agent,
     }
     $('#suspend-btn').removeClass('disabled');
     console.log(options);
-    setInterval(function(){ request(options, callback); }, 3000);
+    interval = setInterval(function () { request(options, callback); }, 3000);
     // request(options, callback);
 });
 
-connector.on('disconnect',(data)=>{
-    connector.stop();
+connector.on('disconnect', (data) => {
+    // connector.stop();
     $('#hook-btn').removeClass('disabled');
     console.log("lost connection with LCU");
     $('#stop-btn').addClass('disabled');
     $('#suspend-btn').addClass('disabled');
     $('#config-btn').addClass('disabled');
-    document.getElementById('message').innerHTML="Lost connection with game client.";
+    document.getElementById('message').innerHTML = "Lost connection with game client.";
 })
 
-function callback(error, response, body){
+function callback(error, response, body) {
     console.log(error);
     console.log(response);
     console.log(body);
     const info = JSON.parse(body);
     console.log(info);
-    if(info.state==="InProgress"){
-        document.getElementById('message').innerHTML="found a match";
+    if (info.state === "InProgress") {
+        document.getElementById('message').innerHTML = "found a match";
     }
 }
 
-$("#hook-btn").click(function(){
-    document.getElementById('message').innerHTML="Looking for game instance...";
+$("#hook-btn").click(function () {
+    document.getElementById('message').innerHTML = "Looking for game instance...";
+    // connector.stop();
     connector.start();
     $('#hook-btn').addClass('disabled');
     $('#stop-btn').removeClass('disabled');
     $('#config-btn').addClass('disabled');
 });
 
-$('#stop-btn').click(function(){
-    connector.stop();
-    document.getElementById('message').innerHTML="Stopped looking for game instance.";
+$('#stop-btn').click(function () {
+    // connector.stop();
+    clearInterval(interval);
+    document.getElementById('message').innerHTML = "Stopped looking for game instance. (requests)";
     $('#hook-btn').removeClass('disabled');
     $('#stop-btn').addClass('disabled');
     $('#config-btn').removeClass('disabled');
