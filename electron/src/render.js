a32f4c55d3db8e5a1719c0e01d5e8df1da7ca868
Jacob <jacob.michels2025@gmail.com>
2020-04-20 15:41:38 -0400

    cleaned up and commented

--- electron/src/render.js
+++ electron/src/render.js
@@ -1,16 +1,27 @@
-const LCUConnector = require('lcu-connector');
-const https = require('https');
-let connector;
 const Base64 = require('js-base64').Base64;
-const request = require('postman-request');
 const { BrowserWindow } = require('electron').remote;
-const path = require('path');
+const customTitlebar = require('custom-electron-titlebar');
 const fs = require('fs');
+const https = require('https');
+const LCUConnector = require('lcu-connector');
+const request = require('postman-request');
 
+//global interval variable for starting and stopping polling
 var interval;
 
-const customTitlebar = require('custom-electron-titlebar');
+//global variable for accessing LCU
+let connector;
 
+//global variable to store pushed_id
+let id;
+
+//global variable to store info about current client session
+let LCU;
+
+//global variable to store current timer on ready check. initialized to value larger than max timer
+let prompt_timer = 15;
+
+//create titlebar
 new customTitlebar.Titlebar({
     backgroundColor: customTitlebar.Color.fromHex('#333333'),
     menu: null,
@@ -20,14 +31,14 @@ new customTitlebar.Titlebar({
 $(document).ready(function () {
     $('#stop-btn').addClass('disabled');
     $('#suspend-btn').addClass('disabled');
-    updateID();
+    readConfig();
 })
 
-updateID = () => {
+//read config file and set global id
+readConfig = () => {
     fs.readFile(__dirname + '/../config.json', (err, data) => {
         if (err) {
             id = 0;
-            // let html = $('#message').html()+'Please set your pushed ID in the config menu';
             $('#message').html('Please set your pushed ID in the config menu');
         }
         else {
@@ -37,57 +48,44 @@ updateID = () => {
     })
 }
 
-// process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
-
-let globalData;
-
-let prompt_timer = 100;
-let accepted=false;
-
-function callback(error, response, body) {
-    // console.log(error);
-    // console.log(response);
-    // console.log(body);
+//call back to "check if in queue" request
+readyCheck=(error, response, body) => {
     let info;
-    try{
+    try {
         info = JSON.parse(body);
     }
-    catch(e){
+    catch (e) {
         console.warn(e);
     }
-    // console.log(info);
     console.log(prompt_timer);
-    if (info.state === "InProgress") {
-        // console.log("timer: "+info.timer+" last_time:"+prompt_timer);
+    if (info.state === "InProgress") {      //id this is true, the ready check is available
         if (info.timer > prompt_timer) {
             prompt_timer = info.timer;
             if (prompt_timer === 10) {
-                //accept the queue here
-                if(!accepted){
-                    var agentOptions;
-                    var agent;
-                    let pw = Base64.encode("riot:" + globalData.password);
-    
-                    agentOptions = {
-                        rejectUnauthorized: false
-                    };
-                    agent = new https.Agent(agentOptions);
-                    const acceptOptions = {
-                        url: 'https://127.0.0.1:' + globalData.port + '/lol-matchmaking/v1/ready-check/accept',
-                        headers: {
-                            'User-Agent': 'postman-request',
-                            'Accept': 'application/json',
-                            'Authorization': 'Basic ' + pw,
-                        },
-                        agent: agent,
-                    };
-                    request.post(acceptOptions,acceptCallback);
-                    clearInterval(interval);
-                }   
+                //send request to accept the queue
+                var agentOptions;
+                var agent;
+                let pw = Base64.encode("riot:" + LCU.password);
+                agentOptions = {
+                    rejectUnauthorized: false   //needs to do this since LCU api is http
+                };
+                agent = new https.Agent(agentOptions);
+                const acceptOptions = {
+                    url: 'https://127.0.0.1:' + LCU.port + '/lol-matchmaking/v1/ready-check/accept',
+                    headers: {
+                        'User-Agent': 'postman-request',
+                        'Accept': 'application/json',
+                        'Authorization': 'Basic ' + pw,
+                    },
+                    agent: agent,
+                };
+                request.post(acceptOptions, acceptCallback);        //accept ready check 
+                clearInterval(interval);    //stop polling for queue prompt
+
             }
         }
         else if (info.timer < prompt_timer) {
-            //new queue prompt
+            //new queue prompt, so send notification to phone via firebase
             prompt_timer = info.timer;
             document.getElementById('message').innerHTML = "Match found! Notification sent.";
             $.ajax({
@@ -95,33 +93,28 @@ function callback(error, response, body) {
                 data: { "id": id, "event": "match found" },
                 url: 'https://us-central1-lol-boosted.cloudfunctions.net/sendNotification',
                 success: function (data) {
-                    // console.log("success");
-                    // console.log(data);
                 },
                 error: function (data) {
                     console.log("error in http request to firebase");
                 }
             })
         }
-        // document.getElementById('message').innerHTML = "Match found! Notification sent.";
-        // let fd = new FormData();
-        // fd.append("id",id);
-        // fd.append("event","match found");
 
     }
 }
 
-acceptCallback = (err,response,body) =>{
-    if(err){
+//callback to "accept ready check" request
+acceptCallback = (err, response, body) => {
+    if (err) {
         return console.log(err);
     }
     return console.log("accepted ready check");
 }
 
 $("#hook-btn").click(function () {
-    connector=new LCUConnector();
+    //instantiate connector object and set event listeners
+    connector = new LCUConnector();
     connector.on('disconnect', (data) => {
-        // connector.stop();
         $('#hook-btn').removeClass('disabled');
         console.log("lost connection with LCU");
         $('#stop-btn').addClass('disabled');
@@ -130,19 +123,18 @@ $("#hook-btn").click(function () {
         document.getElementById('message').innerHTML = "Lost connection with game client.";
     });
     connector.on('connect', (data) => {
+        //on connection to the game client, start polling for queue
         console.log(data);
-        globalData=data;
+        LCU = data;
         document.getElementById('message').innerHTML = "Successfully attached to game instance. Will now begin to poll for queue prompt.";
-        // $('#hook-btn').addClass('disabled');
         let pw = Base64.encode("riot:" + data.password);
-        console.log(pw);
         var agentOptions;
         var agent;
-    
+
         agentOptions = {
             rejectUnauthorized: false
         };
-    
+
         agent = new https.Agent(agentOptions);
         const options = {
             url: 'https://127.0.0.1:' + data.port + '/lol-matchmaking/v1/ready-check',
@@ -153,77 +145,37 @@ $("#hook-btn").click(function () {
             },
             agent: agent,
         }
-        // $('#suspend-btn').removeClass('disabled');
         console.log(options);
-        interval = setInterval(function () { request(options, callback); }, 50);
-        // request(options, callback);
+        interval = setInterval(function () { request(options, readyCheck); }, 50);      //TODO make interval configurable
     });
     connector.start();
-    // $('#hook-btn').addClass('disabled');
     $('#stop-btn').removeClass('disabled');
     $('#config-btn').addClass('disabled');
     document.getElementById('message').innerHTML = "Looking for game instance...";
     BrowserWindow.getFocusedWindow().setSize(500, 265);
-    console.log(connector);
-    // connector.stop();
-    // $.ajax({
-    //     type:'POST',
-    //     data:{"id":id,"event":"match found"},
-    //     url:'https://us-central1-lol-boosted.cloudfunctions.net/sendNotification',
-    //     success:function(data){
-    //         console.log("success");
-    //         console.log(data);
-    //     },
-    //     error:function(data){
-    //         console.log("error in http request to firebase");
-    //     }
-    // })
-
 });
 
-$('#config-btn').click(function () {
+$('#config-btn').click(function () {        //show configuration options
     BrowserWindow.getFocusedWindow().setSize(500, 340);
     $('#message').html('<div class="input-field"><input id="pushed-id-field" class="white-text" type="text"><label for="pushed-id-field">Pushed ID</label><a onclick="confirm()" class="waves-effect red waves-light btn">save</a></div>');
 })
 
-confirm = () => {
-
-    let id = $('#pushed-id-field').val();
-    let config = { 'pushed_id': id };
-
-    // $('#pushed-id-field').val();
-    // console.log(fs);
-    // console.log()
-    // fs.readFile(__dirname + '/../foo.bar');
+confirm = () => {       //save id to file
+    let pushed_id = $('#pushed-id-field').val();
+    let config = { 'pushed_id': pushed_id };
     fs.writeFile(__dirname + '/../config.json', JSON.stringify(config), () => {
         $('#message').html('Configuration saved');
-        updateID();
+        readConfig();
     })
     console.log(config);
     BrowserWindow.getFocusedWindow().setSize(500, 265);
 }
 
 $('#stop-btn').click(function () {
-    connector.stop();
+    connector.stop();       //stop polling for queue
     clearInterval(interval);
     document.getElementById('message').innerHTML = "Stopped looking for game instance. (requests)";
     $('#hook-btn').removeClass('disabled');
     $('#stop-btn').addClass('disabled');
     $('#config-btn').removeClass('disabled');
 });
-
-// $('#exit-btn').click(function(){
-//     let window = remote.getCurrentWindow();
-//     window.close();
-// })
-
-// console.log(connector);
-
-// var windowTopBar = document.createElement('div')
-// windowTopBar.style.width = "100%"
-// windowTopBar.style.height = "32px"
-// windowTopBar.style.backgroundColor = "#2b61d6"
-// windowTopBar.style.position = "relative"
-// windowTopBar.style.top = windowTopBar.style.left = 0
-// windowTopBar.style.webkitAppRegion = "drag"
-// document.body.prepend(windowTopBar);
