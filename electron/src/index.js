8daf34d7e3c02667f80b72f513c5b5af9c27d735
Jacob <jacob.michels2025@gmail.com>
2020-04-23 22:38:34 -0400

    first real release. auto update works

--- electron/src/index.js
+++ electron/src/index.js
@@ -1,6 +1,8 @@
 const { app, BrowserWindow, ipcMain } = require('electron');
 const path = require('path');
-require('update-electron-app')()
+require('update-electron-app')({
+  repo:'jacobmichels/boosted'
+})
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
